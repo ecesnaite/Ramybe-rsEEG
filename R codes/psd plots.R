db974000ed7570481cb8d007d3d5a93e5984954e
Elena Cesnaite <e.cesnaite@gmail.com>
2023-10-30 09:29:56 +0100

    updated figure colors

--- R codes/psd plots.R
+++ R codes/psd plots.R
@@ -7,31 +7,33 @@ library(ggplot2)
 library(tidyr)
 library(RColorBrewer)
 data_long = gather(myData[,c(1:4)], factor_key = T)
-data_long$y <- myData$freq
+data_long$freq <- myData$freq
+data_long$group <- 'dotted'
 
 apriodic_long <- gather(aperiodicData[,c(1:4)], factor_key = T)
 apriodic_long$freq <- aperiodicData$freq
-
+apriodic_long$group <- 'solid'
+both_long <- rbind(apriodic_long,data_long)
 
 mycolors_all <- brewer.pal(n = 4, "Set3")
 mycolors_all <- mycolors_all[c(1,2,4,3)]
+mycolors_all <- c("#8DD3C7", "#FFFFB3", "#FB8072", "#BEBADA","#8DD3C7", "#FFFFB3", "#FB8072", "#BEBADA") #duplicate for two plots
 
-tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_all_avg.png", units="in", width=4, height=3, res=300)
+names(mycolors_all) <- c("NCF", "NCL", "IUD", "OC","ncf","ncg", "uid","oc") 
 
- ggplot(data_long, aes(x=y, y= log(value), group=key)) +
-  geom_line(aes(color=key), size = 1)+ 
-  geom_line(data = apriodic_long, aes(x=freq, y= value, group=key), size = .5, linetype = "dashed")+
+tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_all_avg.png", units="in", width=4.5, height=3, res=300)
+
+  ggplot(both_long, aes(x=freq, y= log(value), group=key, linetype = group)) +
+  geom_line(aes( color=key), size = 1)+ 
   theme_classic() + 
-  scale_colour_manual(values=mycolors_all, labels = c("NCF", "NCG", "UID", "OC")) + 
-  labs(x="Frequency (Hz)", y = "log Power", color = "Groups")+
-  theme(axis.text=element_text(size=12), legend.text = element_text(size=12), legend.title = element_text(size=12))
-
-ggplot(apriodic_long, aes(x=freq, y= value, group=key)) +
-  geom_line(aes(color=key), size = .5, linetype = "dashed")+ 
   scale_colour_manual(values=mycolors_all) + 
-  theme_classic() 
-
+   scale_fill_manual( values = mycolors_all,limits = c("NCF", "NCL", "IUD", "OC"))+
+  labs(x="Frequency (Hz)", y = "log Power", color = "Groups",linetype=element_blank())+
+  theme(axis.text=element_text(size=12),axis.title=element_text(size=14),
+        legend.text = element_text(size=12), legend.title = element_text(size=14))
+ 
 dev.off()
+
 
 ## Each group separately ##
 
@@ -51,13 +53,15 @@ my_green = brewer.pal(n = 9, "Greens")[3:9] #there are 9, I exluded the two ligh
 green_palette = colorRampPalette(c(my_green[1], my_green[4], my_green[6]), space = "Lab")
 my_green2 = green_palette(34)
 
-tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_ncf.png", units="in", width=4, height=3, res=300)
+tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_ncf.png", units="in", width=3.5, height=3, res=300)
 
 ggplot(t_ncf_long, aes(x=freq, y= log(value), group=key)) +
   geom_line(aes(color=key), alpha=0.2) + 
-  geom_line(data = nfc_mean,aes(x=y, y= log(value)), alpha = .8, size = 1) +
+  geom_line(data = nfc_mean,aes(x=freq, y= log(value)), alpha = .8, size = 1) +
   scale_colour_manual(values=my_green2)+theme_classic() + 
-  labs(title = "NCF", x="Frequency (Hz)", y = "log Power") + theme(legend.position="none",axis.text=element_text(size=12))
+  labs(title = "NCF", x="Frequency (Hz)", y = "log Power") + 
+  theme(legend.position="none",axis.text=element_text(size=12),axis.title=element_text(size=14))+
+  ylim(-4,6)
 dev.off()
 
 ## NCG Plots ##
@@ -72,13 +76,16 @@ my_yellow = brewer.pal(n = 9, "YlOrBr")[1:7] #there are 9, I exluded the two lig
 yellow_palette = colorRampPalette(c(my_yellow[1], my_yellow[4], my_yellow[6]), space = "Lab")
 my_yellow2 = yellow_palette(35)
 
-tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_ncg.png", units="in", width=4, height=3, res=300)
+tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_ncg.png", units="in", width=3.5, height=3, res=300)
 
 ggplot(t_ncg_long, aes(x=freq, y= log(value), group=key)) +
   geom_line(aes(color=key), alpha=0.2) + 
-  geom_line(data = ncg_mean,aes(x=y, y= log(value)), alpha = .8, size = 1) +
+  geom_line(data = ncg_mean,aes(x=freq, y= log(value)), alpha = .8, size = 1) +
   scale_colour_manual(values=my_yellow2)+theme_classic() + 
-  labs(title = "NCG", x="Frequency (Hz)", y = "log Power") + theme(legend.position="none",axis.text=element_text(size=12))
+  labs(title = "NCL", x="Frequency (Hz)", y = "log Power") + 
+  theme(legend.position="none",axis.text=element_text(size=12),axis.title=element_text(size=14))+
+  ylim(-4,6)
+
 dev.off()
 
 ## UID Plots ##
@@ -93,13 +100,15 @@ my_orange = brewer.pal(n = 9, "OrRd")[3:9] #there are 9, I exluded the two light
 orange_palette = colorRampPalette(c(my_orange[1], my_orange[4], my_orange[6]), space = "Lab")
 my_orange2 = orange_palette(30)
 
-tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_uid.png", units="in", width=4, height=3, res=300)
+tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_uid.png", units="in", width=3.5, height=3, res=300)
 
 ggplot(t_uid_long, aes(x=freq, y= log(value), group=key)) +
   geom_line(aes(color=key), alpha=0.2) + 
-  geom_line(data = uid_mean,aes(x=y, y= log(value)), alpha = .8, size = 1) +
+  geom_line(data = uid_mean,aes(x=freq, y= log(value)), alpha = .8, size = 1) +
   scale_colour_manual(values=my_orange2)+theme_classic() + 
-  labs(title = "UID", x="Frequency (Hz)", y = "log Power") + theme(legend.position="none",axis.text=element_text(size=12))
+  labs(title = "IUD", x="Frequency (Hz)", y = "log Power") + 
+  theme(legend.position="none",axis.text=element_text(size=12),axis.title=element_text(size=14))+
+  ylim(-4,6)
 
 dev.off()
 
@@ -116,12 +125,14 @@ my_purple = brewer.pal(n = 9, "BuPu")[3:9] #there are 9, I exluded the two light
 purple_palette = colorRampPalette(c(my_purple[1], my_purple[4], my_purple[6]), space = "Lab")
 my_purple2 = purple_palette(36)
 
-tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_oc.png", units="in", width=4, height=3, res=300)
+tiff("/Users/ecesnaite/Desktop/BuschLab/Ramybe-rsEEG/figures/psd_oc.png", units="in", width=3.5, height=3, res=300)
 
 ggplot(t_oc_long, aes(x=freq, y= log(value), group=key)) +
   geom_line(aes(color=key), alpha=0.2) + 
-  geom_line(data = oc_mean,aes(x=y, y= log(value)), alpha = .8, size = 1) +
-  scale_colour_manual(values=my_orange2)+theme_classic() + 
-  labs(title = "OC", x="Frequency (Hz)", y = "log Power") + theme(legend.position="none",axis.text=element_text(size=12))
+  geom_line(data = oc_mean,aes(x=freq, y= log(value)), alpha = .8, size = 1) +
+  scale_colour_manual(values=my_purple2)+theme_classic() + 
+  labs(title = "OC", x="Frequency (Hz)", y = "log Power") + 
+  theme(legend.position="none",axis.text=element_text(size=12),axis.title=element_text(size=14))+
+  ylim(-4,6)
 
 dev.off()
